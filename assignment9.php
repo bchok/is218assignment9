7c1654e8dc8bfd1c484e003bf2b1117afe0e9310
bchok <bchok91@gmail.com>
2016-11-20 10:06:08 -0500

    reworked everything into a class. for some reason not receiving output for my error handling

--- assignment9.php
+++ assignment9.php
@@ -1,24 +1,37 @@
 <?php
 
-    $myarray = array("Brian, Holly, Debbie, Kenny", "TJ, Erin, Kate, Mo");
-
-    try {
-    $file = fopen("writeCSV.csv", "w");
-    } catch (Exception $e){
-        echo 'The file was not found: '. $e->getMessage() ."\n";
+class MyCSVWriter{
+    
+    public function openCSV($file){
+        try {
+         fopen($file, "w");
+        } catch (Exception $e){
+            echo 'The file was not found: '. $e->getMessage() ."\n";
+        }
     }
 
-    try{
-    foreach ($arrayLine as $line){
-        fputcsv($file, explode(',', $line));
-    }
-    }catch(Exception $e){
-        echo 'Unable to write to file: ', $e->getMessage(), "\n";
-    }
-
+    public function writeToCSV($file, $userarray){
+        try {
+            fopen($file, "w");
+        } catch (Exception $e){
+            echo 'The file was not found: '. $e->getMessage() ."\n";
+        }
+        try{
+            foreach ($userarray as $line){
+                fputcsv($file, explode(',', $line));
+        }
+        }catch(Exception $e){
+            echo 'Unable to write to file: ', $e->getMessage(), "\n";
+        }
     fclose($file);
+    }
+}
 
-    echo 'the code utilizes a try and catch block to see if the exception gets thrown<br>';
-    echo 'i cannot get the exception to be thrown even though the file does not exist<br>';
-    echo 'therefore my custom error message for the exception is not being displayed<br>';
+$myWriter = new MyCSVWriter();
+$myarray = array("Brian, Holly, Debbie, Kenny", "TJ, Erin, Kate, Mo");
+$myWriter->openCSV("myCSV.csv");
+$myWriter->writeToCSV("myCSV.csv", $myarray);
+
+
+
 ?>
